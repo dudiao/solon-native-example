12c06ce884a1aafad5ecbaf14d9b9506ea4c6981
西东 <noear@live.cn>
2024-08-01 09:13:12 +0800

    Solon 升为 2.7.6 (#12)

--- src/main/java/com/github/dudiao/solon/nativex/example/controller/TestController.java
+++ src/main/java/com/github/dudiao/solon/nativex/example/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.github.dudiao.solon.nativex.example.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.solon.plugins.pagination.Page;
 import com.github.dudiao.solon.nativex.example.service.TestService;
@@ -14,8 +13,6 @@ import org.noear.solon.annotation.Param;
 import org.noear.solon.annotation.Path;
 import org.noear.solon.core.handle.ModelAndView;
 
-import java.util.List;
-
 /**
  * @author songyinyin
  * @since 2023/4/14 14:34
@@ -23,43 +20,44 @@ import java.util.List;
 @Controller
 public class TestController {
 
-  @Inject
-  private TestService testService;
-  @Inject
-  UserMapper userMapper;
+    @Inject
+    private TestService testService;
 
-  @Mapping("/hello")
-  public String hello(@Param(defaultValue = "world") String name) {
-    return "Hello " + name;
-  }
+    @Inject
+    UserMapper userMapper;
 
-  @Mapping("/hello/tml")
-  public ModelAndView helloTml(@Param(defaultValue = "world") String name) {
-    return new ModelAndView("hello.ftl").put("name", name);
-  }
+    @Mapping("/hello")
+    public String hello(@Param(defaultValue = "world") String name) {
+        return "Hello " + name;
+    }
 
-  @Mapping("/cache/say")
-  public String say(String msg) {
-    return testService.say(msg);
-  }
+    @Mapping("/hello/tml")
+    public ModelAndView helloTml(@Param(defaultValue = "world") String name) {
+        return new ModelAndView("hello.ftl").put("name", name);
+    }
+
+    @Mapping("/cache/say")
+    public String say(String msg) {
+        return testService.getClass().getName() + "\n::" + testService.say(msg);
+    }
 
 
-  @Mapping("/user/{id}")
-  public User userById(@Path("id") Long id) {
-    return userMapper.selectById(id);
-  }
+    @Mapping("/user/{id}")
+    public User userById(@Path("id") Long id) {
+        return userMapper.selectById(id);
+    }
 
-  @Mapping("/userByName/{name}")
-  public User user(@Path("name") String name) {
-    LambdaQueryWrapper<User> query = new LambdaQueryWrapper<>();
-    query.eq(User::getName, name);
-    return userMapper.selectOne(query);
-  }
+    @Mapping("/userByName/{name}")
+    public User user(@Path("name") String name) {
+        LambdaQueryWrapper<User> query = new LambdaQueryWrapper<>();
+        query.eq(User::getName, name);
+        return userMapper.selectOne(query);
+    }
 
-  @Mapping("/user/page/{num}")
-  public IPage<User> userPage(@Path("num") Long num) {
-    LambdaQueryWrapper<User> query = new LambdaQueryWrapper<>();
-    query.ge(User::getUserId, 10);
-    return userMapper.selectPage(Page.of(num, 5), query);
-  }
+    @Mapping("/user/page/{num}")
+    public IPage<User> userPage(@Path("num") Long num) {
+        LambdaQueryWrapper<User> query = new LambdaQueryWrapper<>();
+        query.ge(User::getUserId, 10);
+        return userMapper.selectPage(Page.of(num, 5), query);
+    }
 }
